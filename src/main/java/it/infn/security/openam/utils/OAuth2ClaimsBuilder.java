8fa927c7a3a3b51d88d3495951848bfebac0a553
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-05-18 09:26:57 +0200

    New claims builder class

---
+++ src/main/java/it/infn/security/openam/utils/OAuth2ClaimsBuilder.java
@@ -0,0 +1,15 @@
+package it.infn.security.openam.utils;
+
+import com.iplanet.sso.SSOException;
+import com.iplanet.sso.SSOToken;
+import com.sun.identity.shared.debug.Debug;
+import org.forgerock.oauth2.core.UserInfoClaims;
+
+public class OAuth2ClaimsBuilder {
+
+    public static UserInfoClaims getUserInfoClaims(SSOToken token, Debug logger) 
+        throws SSOException {
+        return null;
+    }
+
+}
